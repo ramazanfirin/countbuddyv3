9f903196a95b4e6b3d694696b0beaa4ee4aeff7d
ramazanfirin <ramazan_firin@hotmail.com>
2018-12-29 02:47:32 +0300

    Initial application generated by JHipster-4.13.0

---
+++ src/main/java/com/mastertek/countbuddy/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.mastertek.countbuddy.config.audit;
---
+++ src/main/java/com/mastertek/countbuddy/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.mastertek.countbuddy.repository;
+
+import com.mastertek.countbuddy.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
